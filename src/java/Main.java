266f1ecf710639c63941122fe6b1edf673d7df38
user1234 <DABurylev@sberbank.ru>
2020-10-26 15:21:25 +0300

    Now you have special menu that shows all available options in application

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,33 +1,41 @@
 import model.Person;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         String exit = "open";
         Scanner scanner = new Scanner(System.in);
         List<Person> list = new ArrayList<>();
+        System.out.println("Menu: ");
+        System.out.println("1. Add ");
+        System.out.println("2. Show ");
+        System.out.println("3. Exit ");
         while (!exit.equals("exit")) {
-            String firstName = scanner.next();
-            if (firstName.equals("exit")) {
-                exit = "exit";
-            } else {
-                String secondName = scanner.next();
-                if (secondName.equals("exit")) {
-                    exit = "exit";
-                } else {
-                    Person person = new Person(firstName, secondName);
-                    list.add(person);
+            String line = scanner.nextLine();
+            args = line.split(" ");
+            if (args.length == 3 || args.length == 1) {
+                switch (args[0]) {
+                    case "Add":
+                        list.add(new Person(args[1], args[2]));
+                        break;
+                    case "Show":
+                        Collections.sort(list);
+                        for (Person person : list) {
+                            System.out.println(person.getFirstName() + " " + person.getSecondName());
+                        }
+                        break;
+                    case "Exit":
+                        exit = "exit";
+                        break;
+                    default:
+                        System.out.println("No action " + args[0] + " in menu");
+                        break;
                 }
+            } else {
+                System.out.println("Wrong number of arguments");
             }
         }
         scanner.close();
-        Collections.sort(list);
-        for (Person person : list) {
-            System.out.println(person.getFirstName() + " " + person.getSecondName());
-        }
     }
 }
